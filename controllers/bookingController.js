afb405e34a6f980f2a027c05a5468d6e816fb846
OptimusRahul <rockerahul97@gmail.com>
2020-01-11 15:06:27 +0530

    Bug fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ const createBookingCheckout = async session => {
 };
 
 exports.webhookCheckOut = (req, res, next) => {
-    const signature = req.headers('stripe-signature');
+    const signature = req.headers['stripe-signature'];
     let event;
     try {
         event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
